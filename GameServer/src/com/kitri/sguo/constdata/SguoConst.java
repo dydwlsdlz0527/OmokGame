d32244bc903ecee207f1283fc4cb7af795dfff15
dydwlsdlz0527 <dydwlsdlz0527@gmail.com>
2019-07-01 07:54:21 +0900

    게임 서버 설정

--- GameServer/src/com/kitri/sguo/constdata/SguoConst.java
+++ GameServer/src/com/kitri/sguo/constdata/SguoConst.java
@@ -2,5 +2,7 @@ package com.kitri.sguo.constdata;
 
 public class SguoConst {
 	
+	//게임 접속 포트 번호
+	public static final int UPORT = 7777;
 	
 }
--- GameServer/src/com/kitri/sguo/server/SguoServer.java
+++ GameServer/src/com/kitri/sguo/server/SguoServer.java
@@ -3,15 +3,26 @@ package com.kitri.sguo.server;
 import java.io.IOException;
 import java.net.ServerSocket;
 
-public class SguoServer {
+import com.kitri.sguo.constdata.SguoConst;
+
+public class SguoServer implements Runnable{
 
 	ServerSocket ss = null;
 	
+	
+	
 	public SguoServer() {
 		try {
-			ss = new ServerSocket();
+			ss = new ServerSocket(SguoConst.UPORT);
+			System.out.println("접속 대기...");
 		}catch(IOException e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	public void run() {
+		
+		
 	}
 }
